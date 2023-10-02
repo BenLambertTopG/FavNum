af259302956570824c076227eb96de032cf03178
Ben Lambert <114227087+BenLambertTopG@users.noreply.github.com>
2023-10-02 10:03:00 +0100

    Original Upload
    Favourite number task from Udemy Course

---
+++ Favourite Number/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int num = 0;
+
+std::cout <<"What is your favourite number? " << std::endl;
+std::cin >> num;
+std::cout << "Amazing " << num << " is my favourite number too!";
+
+return 0;
+
+}
